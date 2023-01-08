8d26155776044831f58d62a5bf408f93d69a5712
Ostap Sulyk <ostapsulyk@gmail.com>
2023-01-08 01:57:19 -0500

    Hooray i can read from the file now 🎉

---
+++ src/main/scala/year_2015/day1/Task1.scala
@@ -0,0 +1,11 @@
+package year_2015.day1
+
+import scala.io.Source
+
+object Task1 extends App {
+  private val fileName = "src/main/scala/year_2015/day1/input.txt"
+  private val buffer = Source.fromFile(fileName)
+
+
+  buffer.close()
+}
